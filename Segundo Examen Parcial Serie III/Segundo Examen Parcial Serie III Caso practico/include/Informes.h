f3f85bf5e5de2eae50f7350c591c9e1fdf10b547
Kevin González <kgonzalezs18@miumg.edu.gt>
2023-04-19 11:47:00 -0600

    Agregando funcionalidad a la clase informe

--- Segundo Examen Parcial Serie III/Segundo Examen Parcial Serie III Caso practico/include/Informes.h
+++ Segundo Examen Parcial Serie III/Segundo Examen Parcial Serie III Caso practico/include/Informes.h
@@ -1,16 +1,24 @@
 #ifndef INFORMES_H
 #define INFORMES_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 class Informes
 {
     public:
         Informes();
 
+        void imprimirInforme();
 
     protected:
 
     private:
+        int idReservacion;
+
 };
 
 #endif // INFORMES_H
--- Segundo Examen Parcial Serie III/Segundo Examen Parcial Serie III Caso practico/src/Informes.cpp
+++ Segundo Examen Parcial Serie III/Segundo Examen Parcial Serie III Caso practico/src/Informes.cpp
@@ -5,3 +5,26 @@ Informes::Informes()
     //ctor
 }
 
+void imprimirInforme()
+{
+    int idPersona;
+    system("cls");
+    fstream file;//Se declara una variable "file" de tipo fstream para abrir el archivo.
+    string line;//Se declara una variable "line" de tipo string para almacenar las líneas leídas del archivo.
+    cout<<"\n------------------------- Informes de clientes -------------------------"<<endl;
+    cout<<"\nIngrese Id de la Persona que quiere buscar: ";
+    cin>>idPersona;
+    /*file >> idReservacion;
+    file.open("Informes.txt", ios::in);//Se abre el archivo en modo lectura "ios::in" y se verifica si se pudo abrir correctamente.
+    if (file.is_open()) {
+        if(participant_id==id)
+			{
+            cout <<"Id Persona: "<<id<<endl;
+
+			}
+        file.close();//Se cierra el archivo "file.close();".
+    } else {
+        cout << "No se pudo abrir el archivo" << endl;
+    }*/
+}
+
